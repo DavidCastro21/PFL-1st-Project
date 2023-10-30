629c2daed8611ddbaec23336886a8e372108270c
up202108763 <up202108763@edu.fe.up.pt>
2023-10-30 00:25:48 +0000

    pieces

--- src/data.pl
+++ src/data.pl
@@ -2,21 +2,32 @@
 
 :- dynamic difficulty/2.
 
-:- dynamic nonblock/2.
+:- dynamic nonblock/1.
 
 board([
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty],
-        [empty,     empty,      empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty,     empty]
+        [nonblock,     nonblock,      nonblock,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     nonblock,     nonblock,     nonblock],
+        [nonblock,     nonblock,      nonblock,     empty,     nonblock,     black,     nonblock,     black,     nonblock,     black,     nonblock,     black,     nonblock,     empty,     nonblock,     nonblock,     nonblock],
+        [nonblock,     nonblock,      empty,     nonblock,     black,     nonblock,     empty,     nonblock,     black,     nonblock,     empty,     nonblock,     black,     nonblock,     empty,     nonblock,     nonblock],
+        [nonblock,     empty,      nonblock,     black,     nonblock,     black,     nonblock,     black,     nonblock,     black,     nonblock,     black,     nonblock,     black,     nonblock,     empty,     nonblock],
+        [empty,     nonblock,      empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty],
+        [nonblock,     empty,      nonblock,     white,     nonblock,     white,     nonblock,     white,     nonblock,     white,     nonblock,     white,     nonblock,     white,     nonblock,     empty,     nonblock],
+        [nonblock,     nonblock,      empty,     nonblock,     white,     nonblock,     empty,     nonblock,     white,     nonblock,     empty,     nonblock,     white,     nonblock,     empty,     nonblock,     nonblock],
+        [nonblock,     nonblock,      nonblock,     empty,     nonblock,     white,     nonblock,     white,     nonblock,     white,     nonblock,     white,     nonblock,     empty,     nonblock,     nonblock,     nonblock],
+        [nonblock,     nonblock,      nonblock,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     empty,     nonblock,     nonblock,     nonblock,     nonblock]
 ]).
 
 
-symbol(nonblock , 'X') :- !.
+
+piece_info(black, player1, black1).
+piece_info(white, player1, white1).
+piece_info(nonblock, neutral).
+piece_info(empty, neutral).
+
+other_player(player1, player2).
+other_player(player2, player1).
+
+
+symbol(black, 'B') :- !.
+symbol(white, 'W') :- !.
+symbol(nonblock , '#') :- !.
 symbol(empty,' ') :- !.
